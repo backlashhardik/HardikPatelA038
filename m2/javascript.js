9a7f1783cf48c3e19c9bc29881e39fdbfeb27fa2
backlashhardik <backlashhardik@gmail.com>
2017-03-22 12:26:29 +0530

    Update javascript.js

--- m2/javascript.js
+++ m2/javascript.js
@@ -5,25 +5,84 @@ document.addEventListener("DOMContentLoaded",
     // Unobtrusive event binding
     document.querySelector("button")
       .addEventListener("click", function () {
+
+               document.getElementById("ques").innerHTML="<h2>" + + "</h2>"
         
-        // Call server to get the name
-        $ajaxUtils
-          .sendGetRequest("data/name.json", 
-            function (res) {
-              var message = 
-                res.firstName + " " + res.lastName
-              if (res.likesChineseFood) {
-                message += " likes Chinese food";
-              }
-              else {
-                message += " doesn't like Chinese food";
-              }
-              message += " and uses ";
-              message += res.numberOfDisplays + 1;
-              message += " displays for coding.";
 
               document.querySelector("#content")
                 .innerHTML = "<h2>" + message + "</h2>";
+        
+        
+        $ajaxUtils
+          .sendGetRequest("jso.json", 
+            function (res) {
+                          
+                          if(document.querySelector("input").value===res.name1){
+                        
+
+                            document.getElementById("ques").innerHTML="<h2>" + res.ques1+ "</h2>";
+                            document.querySelector("input").type="radio";
+                             document.getElementById("i1").name="true";
+                             document.getElementById("i1").name="true"; 
+
+
+                             if(document.getElementById("i1").checked){
+                             
+                              if(document.getElementById("i1").value===res.an1){
+                              
+                                document.getElementById("content").innerHTML="<h2>your answer is correct<h2>"
+                              }
+                             
+                             }else if (document.getElementById("i2").checked) {
+                              if(document.getElementById("i2").value!=res.an1){
+                              
+                                document.getElementById("content").innerHTML="<h2>your answer is incorrect<h2>"
+                              }
+
+                             }
+                           }
+
+
+
+                           if(document.querySelector("input").value===res.name2){
+                        
+
+                            document.getElementById("ques").innerHTML="<h2>" + res.ques2 "</h2>";
+                            document.querySelector("input").type="radio";
+                             document.getElementById("i1").name="true";
+                             document.getElementById("i1").name="true"; 
+
+
+                             if(document.getElementById("i1").checked){
+                             
+                              if(document.getElementById("i1").value===res.ans2){
+                              
+                                document.getElementById("content").innerHTML="<h2>your answer is correct<h2>"
+                              }
+                             
+                             }else if (document.getElementById("i2").checked) {
+                              if(document.getElementById("i2").value!=res.ans2){
+                              
+                                document.getElementById("content").innerHTML="<h2>your answer is incorrect<h2>"
+                              }
+
+                             }
+                           }
+                           
+
+
+                                                    
+
+
+                            
+
+
+
+
+                          
+              
+
+             
             });
       });
   }
